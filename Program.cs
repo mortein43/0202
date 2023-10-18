a439fc50c31c3fc9af4473d264f90a84efffa666
mortein43 <mortein43@gmail.comm>
2023-10-18 18:43:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace HexadecimalDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Шістнадцяткові цифри:");
+            Thread thread = new Thread(PrintHexadecimalDigits);
+            thread.Start();
+        }
+
+        static void PrintHexadecimalDigits()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                if (i < 10)
+                {
+                    Console.Write(i);
+                }
+                else
+                {
+                    char ch = (char)('a' + (i - 10));
+                    Console.Write(ch);
+                }
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
